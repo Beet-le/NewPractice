c6670095d28e132d7305478e4f67e75f4ab18c73
Beet-le <1693332907@qq.com>
2021-07-30 19:56:23 +0800

    Day730

--- src/Day730/Demo02/MyInterfaceDefault.java
+++ src/Day730/Demo02/MyInterfaceDefault.java
@@ -2,4 +2,11 @@ package Day730.Demo02;
 
 public interface MyInterfaceDefault {
     public abstract void methodABS();
+//    public abstract void methodABS2();
+    public default void methodDefault(){
+        System.out.println("这是添加的默认方法");
+    }
+    public default void methodDefault2(){
+        System.out.println("这是添加的默认方法2");
+    }
 }
--- src/Day730/Demo02/MyinterFaceDefaultB.java
+++ src/Day730/Demo02/MyinterFaceDefaultB.java
@@ -1,16 +1,15 @@
 package Day730.Demo02;
 
-public class MyinterFaceDefaultA implements MyInterfaceDefault{
+public class MyinterFaceDefaultB implements MyInterfaceDefault{
 
 
     @Override
     public void methodABS() {
-        System.out.println("实现抽象方法AAA");
+        System.out.println("实现抽象方法BBB");
     }
 
     /*@Override
     public void methodABS2() {
 
     }*/
-
 }
