7575794cb45e016d33e486bdef4c8df4b9da117c
Beet-le <1693332907@qq.com>
2021-08-02 20:39:39 +0800

    Day802

---
+++ src/Day802/Demo03Muliti/Fu.java
@@ -0,0 +1,7 @@
+package Day802.Demo03Muliti;
+
+public class Fu {
+    public void method(){
+        System.out.println("父类方法");
+    }
+}
---
+++ src/Day802/Demo03Muliti/Multi.java
@@ -0,0 +1,9 @@
+package Day802.Demo03Muliti;
+
+public class Multi {
+    public static void main(String[] args) {
+        //多态
+        Fu obj=new Zi();
+        obj.method();
+    }
+}
