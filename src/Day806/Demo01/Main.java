c54c1b5922e9eb3ef2d093cb1b5b4f8440854be7
Beet-le <1693332907@qq.com>
2021-08-06 20:08:48 +0800

    Day806

---
+++ src/Day806/Demo01/Main.java
@@ -0,0 +1,11 @@
+package Day806.Demo01;
+
+import Day804.Demo01.Animal;
+import Day804.Demo01.Cat;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal=new Cat();
+        animal.eat();
+    }
+}
