9a2dc616917e6646238778e639907ed999c7ee0b
Beet-le <1693332907@qq.com>
2021-08-10 18:02:20 +0800

    Day810

--- src/Day810/Demo02/DemoMain.java
+++ src/Day810/Demo02/DemoMain.java
@@ -2,8 +2,20 @@ package Day810.Demo02;
 
 public class DemoMain {
     public static void main(String[] args) {
-        Computer computer=new Computer();
+        Computer computer = new Computer();
         computer.powerOn();
+//        Mouse mouse=new Mouse();
+        USB usbMouse = new Mouse();//向上转型
+        computer.useDevice(usbMouse);
+        Ketbord ketbord=new Ketbord();
+        computer.useDevice(ketbord);
+
         computer.powerOff();
+        System.out.println("=================================");
+        method(10.0);
+        method(10);
+    }
+    public static void method(double num){
+        System.out.println(num);
     }
 }
